6087d049a3548ad754b387cacf4b3a24f2a418a9
Julian Kim <kimjulian@grinnell.edu>
2024-09-23 12:52:40 +0200

    Added tap detection, adjusted styling

--- app/src/main/java/im/juliank/minesweeper/ui/MinesweeperScreen.kt
+++ app/src/main/java/im/juliank/minesweeper/ui/MinesweeperScreen.kt
@@ -1,24 +1,36 @@
 package im.juliank.minesweeper.ui
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun MinesweeperScreen(modifier: Modifier, viewModel: MinesweeperViewModel = viewModel()) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Text("Minesweeper")
-        Text("Flagging: ${viewModel.isFlagMode}")
         FlagCheckbox(modifier = Modifier.fillMaxWidth(), viewModel.isFlagMode, onClick = {newVal ->
             viewModel.isFlagMode = newVal
         })
-        Canvas(modifier = Modifier.fillMaxSize()) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .aspectRatio(1.0f)
+                .pointerInput(key1 = Unit) {
+                    detectTapGestures {
+                        offset -> Log.d("TAG_CLICK", "Click: ${offset.x}, ${offset.y}")
+                    }
+                }
+        ) {
             drawRect(Color.Black, size = this.size)
         }
     }
