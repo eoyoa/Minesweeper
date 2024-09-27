b1e4eda8e8098fb1bc67f04eb4c01ebbec68816b
Julian Kim <kimjulian@grinnell.edu>
2024-09-27 15:36:37 +0200

    Created board and basic click functionality

--- app/src/main/java/im/juliank/minesweeper/ui/MinesweeperScreen.kt
+++ app/src/main/java/im/juliank/minesweeper/ui/MinesweeperScreen.kt
@@ -1,7 +1,7 @@
 package im.juliank.minesweeper.ui
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -10,28 +10,77 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlin.math.floor
 
 @Composable
 fun MinesweeperScreen(modifier: Modifier, viewModel: MinesweeperViewModel = viewModel()) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Text("Minesweeper")
-        FlagCheckbox(modifier = Modifier.fillMaxWidth(), viewModel.isFlagMode, onClick = {newVal ->
-            viewModel.isFlagMode = newVal
-        })
-        Canvas(
-            modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .aspectRatio(1.0f)
-                .pointerInput(key1 = Unit) {
-                    detectTapGestures {
-                        offset -> Log.d("TAG_CLICK", "Click: ${offset.x}, ${offset.y}")
-                    }
+        FlagCheckbox(
+            modifier = Modifier.fillMaxWidth(),
+            viewModel.isFlagMode,
+            onClick = { newVal ->
+                viewModel.isFlagMode = newVal
+            }
+        )
+        MinesweeperBoard(viewModel.board, cellCallback = viewModel::onCellClick)
+    }
+}
+
+@Composable
+fun MinesweeperBoard(board: Array<Array<CellType>>, cellCallback: (CellCoord) -> Unit) {
+    val rows = board.size
+    val cols = board[0].size
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth(0.8f)
+            .aspectRatio(cols.toFloat() / rows)
+            .pointerInput(key1 = Unit) {
+                detectTapGestures {offset ->
+                    val x = floor((offset.x / size.width) * cols).toInt()
+                    val y = floor((offset.y / size.height) * rows).toInt()
+                    cellCallback(CellCoord(x, y))
                 }
-        ) {
-            drawRect(Color.Black, size = this.size)
+            }
+            .border(1.dp, Color.Black)
+    ) {
+        val colWidth = size.width / cols
+        for (i in 1 until cols) {
+            drawLine(
+                color = Color.Black,
+                start = Offset(colWidth * i, 0f),
+                end = Offset(colWidth * i, size.height)
+            )
+        }
+
+        val rowHeight = size.height / rows
+        for (i in 1 until rows) {
+            drawLine(
+                color = Color.Black,
+                start = Offset(0f, rowHeight * i),
+                end = Offset(size.width, rowHeight * i)
+            )
+        }
+
+        for (row in board.indices) {
+            for (col in 0 until board[0].size) {
+                if (board[row][col] != CellType.MINE) continue
+
+                drawCircle(
+                    color = Color.Black,
+                    radius = colWidth / 2,
+                    center = Offset(
+                        (col.toFloat() / board[0].size) * size.width + colWidth / 2,
+                        (row.toFloat() / board.size) * size.height + colWidth / 2
+                    )
+                )
+            }
         }
     }
 }
--- app/src/main/java/im/juliank/minesweeper/ui/MinesweeperViewModel.kt
+++ app/src/main/java/im/juliank/minesweeper/ui/MinesweeperViewModel.kt
@@ -4,7 +4,46 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+enum class CellType {
+    MINE,
+    NONE
+}
+
+class CellCoord(val x: Int, val y: Int)
 
 class MinesweeperViewModel: ViewModel() {
     var isFlagMode by mutableStateOf(false)
+
+    val rows = 16
+    val cols = 9
+    val mines = 3
+
+    var board by mutableStateOf(
+        Array(rows) { Array(cols) { CellType.NONE } }
+    )
+    init {
+        for (i in 1..mines) {
+            var placed = false
+            val rng = Random.Default
+
+            while (!placed) {
+                val row = rng.nextInt(rows)
+                val col = rng.nextInt(cols)
+
+                if (board[row][col] == CellType.MINE) continue
+
+                board[row][col] = CellType.MINE
+                placed = true
+            }
+        }
+    }
+
+    fun onCellClick(coord: CellCoord) {
+        val newBoard = board.copyOf()
+        newBoard[coord.y][coord.x] = CellType.MINE
+
+        board = newBoard
+    }
 }
